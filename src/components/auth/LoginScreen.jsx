4bab5a89d222b9c3608c42e35aba00564069f062
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-07-07 21:40:56 -0500

    [ADD] styles loginscreen

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -6,13 +6,19 @@ export const LoginScreen = () => {
     <>
       <h3 className='auth__title'>Login</h3>
       <form className='auth__form'>
-        <input className='auth__input' type="email" placeholder='nakita@mail.com' name='email' autoComplete='off' />
+        <input
+          className='auth__input'
+          type="email"
+          placeholder='youremail@mail.com'
+          name='email'
+          autoComplete='off'
+        />
         <input className='auth__input' type="password" placeholder='*********' name='password' />
-        <button className='auth__button' type='submit'>Login</button>
+        <button className='button button--primary' type='submit'>Login</button>
         <div className='auth__social-media'>
-          <p>Login with social media</p>
+          <p className='auth__text-media'>Login with social media</p>
           <div className="google-btn grid">
-            <figure className="google-icon-wrapper">
+            <figure className="google-icon-wrapper grid">
                 <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
             </figure>
             <p className="btn-text">
